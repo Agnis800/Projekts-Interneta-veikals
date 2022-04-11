26c3af588f353b38fd0e120d38b8be609d94e698
Agnis800 <78017711+Agnis800@users.noreply.github.com>
2022-04-11 20:51:41 +0300

    Update main.cs

--- main.cs
+++ main.cs
@@ -2,6 +2,9 @@ using System;
 
 class Program {
   public static void Main (string[] args) {
-    Console.WriteLine ("Hello World");
+    Produkts dators = new Produkts("HSG-dhs-4728", 550.00,  "Lenovo Essential V14-IIL", 128.00);
+    Produkts putekļsūcējs = new Produkts("SJD-fus-2948", 458,49, "Roborock S6 MaxV", 0.00);
+    Pasutijums ID46w847 = new Pasutijums("38294hsdq", "Lenovo Essential V14-IIL", 1008,49, "ID46w847");
+    Pircejs Jānis = new Pircejs("321946-17392", "38294hsdq", 5, "1500.00");
   }
 }
