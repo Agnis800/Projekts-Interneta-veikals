5e68f6149692f5305b993cd9205405e4c382a19b
Edgars Polis <93377422+Edgars-DP@users.noreply.github.com>
2022-04-12 19:14:24 +0000

    Noņem relācijas

--- pasutijums.cs
+++ pasutijums.cs
@@ -1,12 +1,12 @@
 using System;
 
-class Pasutijums: Produkts {
+class Pasutijums {
   public string KlientaID { get; set; }
   public string Preces { get; set; }
   public double Cena { get; set; }
   public string PasutijumaID { get; set; }
 
-  public Pasutijums(string klientaID, string preces, double cena, string pasutijumaID) : base (cena) {
+  public Pasutijums(string klientaID, string preces, double cena, string pasutijumaID) {
     this.KlientaID = klientaID;
     this.Preces = preces;
     this.Cena = cena;
--- pircejs.cs
+++ pircejs.cs
@@ -1,12 +1,12 @@
 using System;
 
-class Pircejs : Pasutijums {
+class Pircejs {
   public string PersonasKods { get; set; }
   public int KlientaID { get; set; }
   public int LojalitatesPunkti { get; set; }
   public double Nauda { get; set; }
 
-  public Pircejs(string personasKods, int klientaID, int lojalitatesPunkti, double nauda) : base (klientaID) {
+  public Pircejs(string personasKods, int klientaID, int lojalitatesPunkti, double nauda) {
     this.PersonasKods = personasKods;
     this.KlientaID = klientaID;
     this.LojalitatesPunkti = lojalitatesPunkti;
