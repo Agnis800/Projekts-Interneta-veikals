8153e08e95d0c1d25b102ca5ffffdb04fd5e2291
Edgars Polis <93377422+Edgars-DP@users.noreply.github.com>
2022-04-12 19:38:27 +0000

    Produkta "izņemšana"

--- produkts.cs
+++ produkts.cs
@@ -7,15 +7,22 @@ class Produkts {
   public int Atlaide { get; set; }
   public int Daudzums {get; set;}
 
-  public Produkts(string svitruKods, double cena, string nosaukums, int atlaide) {
+  public Produkts(string svitruKods, double cena, string nosaukums, int atlaide, int daudzums) {
     this.SvitruKods = svitruKods;
     this.Cena = cena;
     this.Nosaukums = nosaukums;
     this.Atlaide = atlaide;
   }
 
-  public void AddProduct() {
-    
+  // "Izņem" produktu no veikala pievienošanai pasūtījumam
+  public Produkts Take(int daudzums) {
+    if(this.Daudzums<daudzums) {
+      // Ja nepietiek naudas visam, pievieno maksimālo iespējamo
+      Console.WriteLine($">> Nepietiek produkta {this.Nosaukums}. Prasīti {daudzums}, bet ir tikai {this.Daudzums}!");
+      daudzums = this.Daudzums;
+    }
+    this.Daudzums -= daudzums;
+    return new Produkts(this.SvitruKods, this.Cena, this.Nosaukums, this.Atlaide, daudzums);
   }
   
 }
